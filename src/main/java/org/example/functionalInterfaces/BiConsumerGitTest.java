5919bb972b49084a04a5862409c5d0b11323192b
nitish093 <nknitish6060@gmail.com>
2024-06-12 02:01:08 +0530

    Added the new java file for java8 feature.

---
+++ src/main/java/org/example/functionalInterfaces/BiConsumerGitTest.java
@@ -0,0 +1,8 @@
+package org.example.functionalInterfaces;
+
+public class BiConsumerGitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Inside the main function...");
+    }
+}
