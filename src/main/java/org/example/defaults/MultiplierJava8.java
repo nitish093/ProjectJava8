36547f80fb886d5a1cb1f7449b299078a1a18c1c
nitish093 <nknitish6060@gmail.com>
2024-06-12 02:04:48 +0530

    Adding the new java multiplier file

---
+++ src/main/java/org/example/defaults/MultiplierJava8.java
@@ -0,0 +1,11 @@
+package org.example.defaults;
+
+public class MultiplierJava8 {
+
+    final static int PAYMENT_NOT_FOUNT_ERROR = 8000;
+
+    //main function
+    public static void main(String[] args) {
+        System.out.println(PAYMENT_NOT_FOUNT_ERROR);
+    }
+}
